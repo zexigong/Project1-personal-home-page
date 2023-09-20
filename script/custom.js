01cdb9cc080e270f9165c7e40c5d16704d90f8e5
zexigong <108034351+zexigong@users.noreply.github.com>
2023-09-19 17:27:39 -0700

    basic

---
+++ script/custom.js
@@ -0,0 +1,15 @@
+// An original JS functionality: Display a welcome message based on the time of the day
+const currentTime = new Date().getHours();
+let welcomeMessage = '';
+
+if (currentTime >= 5 && currentTime < 12) {
+  welcomeMessage = 'Good morning!';
+} else if (currentTime >= 12 && currentTime < 18) {
+  welcomeMessage = 'Good afternoon!';
+} else {
+  welcomeMessage = 'Good evening!';
+}
+
+const greetingElement = document.createElement('div');
+greetingElement.innerHTML = `<h2>${welcomeMessage}</h2>`;
+document.querySelector('.content .container').insertBefore(greetingElement, document.querySelector('.content .container h2'));
